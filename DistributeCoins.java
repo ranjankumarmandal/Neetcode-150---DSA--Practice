d977204551bbfd207f328af11031d8604d07015a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-08-31 22:04:32 +0530

    Advanced Graph: Distributed Coins

---
+++ DistributeCoins.java
@@ -0,0 +1,21 @@
+class Solution {
+    private int moves = 0;
+
+    public int distributeCoins(TreeNode root) {
+        dfs(root);
+        return moves;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        moves += Math.abs(left) + Math.abs(right);
+
+        return root.val + left + right - 1;
+    }
+}
